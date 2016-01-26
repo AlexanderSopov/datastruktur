b0afa801ede44307b90e8f3011e1bf96491779eb
Owe <oskar_willman@live.se>
2016-01-26 10:45:10 +0100

    Created project for lab 1, some development on part 1

---
+++ src/MySqrt.java
@@ -0,0 +1,54 @@
+/**
+ * Created by oskar on 23/01/2016.
+ */
+
+public class MySqrt {
+
+    public static double mySqrtLoop(double x, double epsilon){
+        double upper, lower;
+        double returnValue = Double.NaN;
+
+        if(x >= 0 && x <= 1){
+            upper = 1;
+            lower = x;
+            do {
+                if(Math.pow(((upper+lower)/2), 2) - x > 0){
+                    upper = (upper+lower)/2;
+                }
+                else if(Math.pow(((upper+lower)/2), 2) - x < 0){
+                    lower = (upper+lower)/2;
+                }
+                returnValue = (upper+lower)/2;
+            }
+            while(Math.abs(returnValue) - x < epsilon);
+        }
+        else if(x > 1){
+            upper = x;
+            lower = 1;
+            do {
+                if(Math.pow(((upper+lower)/2), 2) - x > 0){
+                    upper = (upper+lower)/2;
+                }
+                else if(Math.pow(((upper+lower)/2), 2) - x < 0){
+                    lower = (upper+lower)/2;
+                }
+                returnValue = (upper+lower)/2;
+            }
+            while(Math.abs(returnValue) - x < epsilon);
+        }
+        return Math.abs(returnValue);
+    }
+
+    public static double mySqrtRecurse(double x, double epsilon){
+        if(x < 0){
+            return Double.NaN;
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args){
+        System.out.println(mySqrtLoop(5, 0.000001));
+    }
+
+}
