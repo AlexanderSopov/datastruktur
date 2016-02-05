38fd78962a3a44b5fb0d03be26b5d8004eda6549
Alexander Sopov <sopovalexander@gmail.com>
2016-02-05 10:30:48 +0100

    done with lab2a, works properly

--- src/lab2/Lab2a.java
+++ src/lab2/Lab2a.java
@@ -9,23 +9,24 @@ public class Lab2a {
     */
     
     
-    // if all points are erased
+    // escape recursion
     if (poly.length == k*2)
       return poly;
     
     
     int index = 0;
     double smallest = 100;
-    for (int i = 2; i<poly.length-2; i=i+2) {
+
+    for (int i = 2; i<poly.length-2; i+=2) {
       double dist = calculateValue(poly, i);
-      if (dist <= smallest) {
+      if (dist < smallest) {
         smallest = dist;
         index = i;
       }
     }
     
     System.out.println("Index = " + index + "\nPoint = (" + poly[index]+", "+poly[index+1]+")\n"+
-    "Distance = " + smallest);
+    "Distance = " + smallest + "\n\n");
 
     return simplifyShape(reAllocate(poly, index), k);
   }
@@ -36,7 +37,7 @@ public class Lab2a {
 
 
   private static double[] reAllocate(double[] poly, int index) {
-  	System.out.println("index = " + index);
+  	//System.out.println("index = " + index);
     
     
     double[] newPoly = new double[poly.length-2];
@@ -47,27 +48,22 @@ public class Lab2a {
         newPoly[j] = poly[i];
         j++;
       }
-      /*
-      System.out.println("newPoly[first] = (" + newPoly[0] + ", " + newPoly[1] + ")\n" +
-      	"newPoly[last] = (" + newPoly[newPoly.length-2] + ", " + newPoly[newPoly.length-1] + ")\n\n");
-		*/
-	for (int i =0; i<newPoly.length;i+=2)
-		System.out.println("("+newPoly[i]+", " + newPoly[i+1] + ")");
     
     return newPoly;
   }
 
   private static double calculateValue(double[] poly, int i) {
-    double l1 = calculateDistance (poly[i], poly[i+1], poly[i-1], poly[i-2]);
+    double l1 = calculateDistance (poly[i], poly[i+1], poly[i-2], poly[i-1]);
     double l2 = calculateDistance (poly[i], poly[i+1],poly[i+2], poly[i+3]);
-    double l3 = calculateDistance (poly[i-1], poly[i-2],poly[i+2], poly[i+3] );
+    double l3 = calculateDistance (poly[i-2], poly[i-1],poly[i+2], poly[i+3] );
+
     //System.out.println("Distance = " + (l1+l2-l3) + "\n");
         return l1+l2-l3;
   }
 
-  private static double calculateDistance(double v1x, double v1y, double v2x, double v2y) {
-    double x = v2x - v1x;
-    double y = v2y - v1x;
+  private static double calculateDistance(double x1, double y1, double x2, double y2) {
+    double x = x2 - x1;
+    double y = y2 - y1;
     return Math.sqrt(x*x + y*y);
   }
 
