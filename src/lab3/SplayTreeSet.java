14a333a65a90297a8f37f637317b75f4262bf06b
Owe <oskar_willman@live.se>
2016-02-24 22:27:08 +0100

    WOOP FUCKING WOOP MOTHERFUCKER

--- src/lab3/SplayTreeSet.java
+++ src/lab3/SplayTreeSet.java
@@ -6,7 +6,8 @@ package lab3;
 public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<E> {
 
     private int size;
-    private SplayNode root;
+    private SplayNode root = null;
+
 
     private class SplayNode{
         private E value;
@@ -17,36 +18,100 @@ public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<
         }
     }
 
-
     public SplayTreeSet(){
         size = 0;
     }
-
 
     public int size(){
         return size;
     }
 
     public boolean add(E x){
+        SplayNode newNode = new SplayNode(x);
+        if(root == null){
+            root = newNode;
+            size++;
+            return true;
+        }
+
+        //splay(root, x);
+
+        if(this.contains(x)) {
+            System.out.println("\n ADD METHOD RETURNING FALSE");
+            return false;
+        }
+        else{
+            if(x.compareTo(root.value) < 0){
+                newNode.right = root;
+                newNode.left = root.left;
+                root.left = null;
+                root = newNode;
+            }
+            else{
+                newNode.left = root;
+                newNode.right = root.right;
+                root.right = null;
+                root = newNode;
+            }
+        }
+        /*System.out.println("\n BEFORE SPLAY: Root value = " + root.value + "    x = " + x);
+        root = splay(root, x);>*/
+        System.out.println("\n AFTER SPLAY: Root value = " + root.value + "    x = " + x);
         size++;
-        return false;
+        return true;
     }
 
     public boolean remove(E x){
 
+        if(!this.contains(x)){
+            System.out.println("\n REMOVE METHOD RETURNING FALSE");
+            return false;
+        }
+
+        System.out.println(root.value +" == "+ x);
+
+        if(root.right == null && root.left == null)
+            root = null;
+
+        else if(root.right == null){
+            root = root.left;
+            /*System.out.println("\n BEFORE SPLAY: Root value = " + root.value + "    x = " + x);
+            splay(root, x);
+            System.out.println("\n AFTER SPLAY: Root value = " + root.value + "    x = " + x);*/
+        }
+        else if(root.left == null) {
+            root = root.right;
+        }
+        else{
+            SplayNode temp = root.right;
+            root = root.left;
+            root = splay(root, x);
+            root.right = temp;
+        }
         size--;
-        return false;
+        return true;
     }
 
     public boolean contains(E x){
-        return false;
+
+        if(root == null) return false;
+        root = splay(root, x);
+        return(root.value == x);
     }
 
     private SplayNode splay(SplayNode node, E value){
-        if(node == null) return null;
+
+
+        if(node == null){
+            return null;
+        }
 
         if(value.compareTo(node.value) < 0){
-            if(node.left == null) return node;
+
+            if(node.left == null){
+
+                return node;
+            }
             if(value.compareTo(node.left.value) < 0){
                 node.left.left = splay(node.left.left, value);
                 node = rotateRight(node);
@@ -57,29 +122,62 @@ public class SplayTreeSet<E extends Comparable<? super E>> implements SimpleSet<
                     node.left = rotateLeft(node.left);
                 }
             }
+
             if(node.left == null) return node;
-            else                  return rotateLeft(node);
+            else                  return rotateRight(node);
         }
         else if(value.compareTo(node.value) > 0){
-            if(node.right == null) return node;
-            
+
+            if(node.right == null){
+
+                return node;
+            }
+            if(value.compareTo(node.right.value) < 0){
+                node.right.left = splay(node.right.left, value);
+                if(node.right.left != null)
+                    node.right = rotateRight(node.right);
+            }
+            else if(value.compareTo(node.right.value) > 0){
+                node.right.right = splay(node.right.right, value);
+                node = rotateLeft(node);
+            }
+
+            if(node.right == null){
+                return node;
+            }
+            return rotateLeft(node);
         }
 
-        return node;
+        else {
+
+            return node;
+
+        }
     }
 
-    private SplayNode rotateRight(SplayNode node){
-        SplayNode newNode = node.left;
-        node.left = newNode.right;
-        newNode.right = node;
-        return newNode;
+    private SplayNode rotateRight(SplayNode node) {
+        if (node.left == null){
+            return node;
+
+        }
+            else{
+            SplayNode newNode = node.left;
+            node.left = newNode.right;
+            newNode.right = node;
+            return newNode;
+        }
     }
 
     private SplayNode rotateLeft(SplayNode node){
-        SplayNode newNode = node.right;
-        node.right = newNode.left;
-        newNode.left = node;
-        return newNode;
-    }
+        if(node.right == null){
+            return node;
+        }
 
+        else {
+            SplayNode newNode = node.right;
+            node.right = newNode.left;
+            newNode.left = node;
+            return newNode;
+        }
+    }
 }
--- src/lab3/TestSetCorrectness.java
+++ src/lab3/TestSetCorrectness.java
@@ -13,62 +13,73 @@ public class TestSetCorrectness {
     public static void testSet(int n1, int n2, int n3, int n4) throws MyException{
         Random rand = new Random();
         SimpleSet<Integer> set;
-        TreeSet<Integer> refSet = new TreeSet<>();
+        boolean setBool, refSetBool;
 
-        if(n1 == 1){
-            set = new SortedLinkedListSet<>();
-        }
-        else if(n1 == 2){
-            set = new SplayTreeSet<>();
-        }
-        else{
-            throw new MyException("Not a valid n1.");
-        }
+
         for(int i = 1; i <= n2; i++) {
             System.out.println("Iteration nbr: " + i);
+            if(n1 == 1){
+                set = new SortedLinkedListSet<>();
+            }
+            else if(n1 == 2){
+                set = new SplayTreeSet<>();
+            }
+            else{
+                throw new MyException("\nNot a valid n1.");
+            }
+            TreeSet<Integer> refSet = new TreeSet<>();
             for (int k = 1; k <= n3; k++) {
 
-                int randomInt = rand.nextInt(3);
+                int randomInt = rand.nextInt(4);
                 if (randomInt == 0) {
-                    //System.out.println("Running size methods:        Set Size = " + set.size() + "            refSet Size = " + refSet.size());
-                    if (set.size() != refSet.size()) {
-                        throw new MyException("Bug in size method.");
+                    int setSize = set.size();
+                    int refSetSize = refSet.size();
+                    System.out.println("\nRunning size methods:        Set Size = " + setSize + "                refSet Size = " + refSetSize);
+                    if (setSize != refSetSize) {
+                        throw new MyException("\nBug in size method.");
                     }
                 } else if (randomInt == 1) {
                     int n4Rand = rand.nextInt(n4 - 1);
-                    //System.out.println("Running add methods:         Set returns " + set.add(n4Rand) + "           refSet returns " + refSet.add(n4Rand));
-                    if (set.add(n4Rand) != refSet.add(n4Rand)) {
-                        throw new MyException("Bug in add method.");
+                    System.out.println("\nRunning add methods:         Trying to add [" + n4Rand + "] to the sets.");
+                    setBool = set.add(n4Rand);
+                    refSetBool = refSet.add(n4Rand);
+
+                    System.out.println("\nSet returns " + setBool + "            refSet returns " + refSetBool);
+                    if (setBool != refSetBool) {
+                        throw new MyException("\nBug in add method.");
                     }
                 } else if (randomInt == 2) {
                     int n4Rand = rand.nextInt(n4 - 1);
-                    //System.out.println("Running remove methods:         Set returns " + set.remove(n4Rand) + "           refSet returns " + refSet.remove(n4Rand));
-                    if (set.remove(n4Rand) != refSet.remove(n4Rand)) {
-                        throw new MyException("Bug in remove method.");
+                    System.out.println("\nRunning remove methods:     Trying to remove [" + n4Rand + "] from the sets.");
+                    setBool = set.remove(n4Rand);
+                    refSetBool = refSet.remove(n4Rand);
+
+                    System.out.println("\nSet returns " + setBool + "            refSet returns " + refSetBool);
+                    if (setBool != refSetBool) {
+                        throw new MyException("\nBug in remove method.");
                     }
                 } else if (randomInt == 3) {
                     int n4Rand = rand.nextInt(n4 - 1);
-                    //System.out.println("Running contains methods:         Set returns " + set.contains(n4Rand) + "           refSet returns " + refSet.contains(n4Rand));
-                    if (set.contains(n4Rand) != refSet.contains(n4Rand)) {
-                        throw new MyException("Bug in contains method.");
+                    System.out.println("\nRunning contains methods:     Trying to check if [" + n4Rand + "] is part of the sets.");
+                    setBool = set.contains(n4Rand);
+                    refSetBool = refSet.contains(n4Rand);
+
+                    System.out.println("\nSet returns " + setBool + "            refSet returns " + refSetBool);
+                    if (setBool != refSetBool) {
+                        throw new MyException("\nBug in contains method.");
                     }
                 } else {
-                    throw new MyException("Not a valid random Integer.");
+                    throw new MyException("\nNot a valid random Integer.");
                 }
 
-                //Iterator iter = refSet.iterator();
-                //Integer refSetCurrent = refSet.first();
-
-                //while (iter.hasNext()) {
-
-                //}
             }
         }
     }
 
     public static void main(String[] args) {
+
         try {
-            testSet(1, 10000, 100, 20);
+            testSet(2, 10000, 100, 20);
         } catch (MyException e) {
             System.out.println(e.getMessage());
         }
